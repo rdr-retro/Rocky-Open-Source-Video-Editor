62719840cd26ee54582866a08b0865ef11a569b0
Raúl Díaz Gutiérrez <94313309+rdr-retro@users.noreply.github.com>
2026-01-10 12:14:22 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Detect OS to set correct classpath separator
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" ]]; then
-  SEP=";"
-else
-  SEP=":"
-fi
-
-java -Xmx4G -Xms2G -XX:+UseG1GC -XX:MaxGCPauseMillis=10 -Dsun.java2d.metal=true -Dapple.awt.graphics.UseQuartz=true --enable-native-access=ALL-UNNAMED -cp "lib/*${SEP}bin_user" rocky.app.RockyMain
